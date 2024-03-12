7907f9ea0f008e750ac36ceb791a66413a6416da
Nadelele <nadele@gmail.com>
2024-03-12 15:06:58 +0200

    Added formatting and comments

--- src/telran/util/arraysInt.java
+++ src/telran/util/arraysInt.java
@@ -5,39 +5,41 @@ import java.util.Arrays;
 public class arraysInt {
 	public static int[] addNumber(int[] array, int number) {
 		int[] arrayNew = Arrays.copyOf(array, array.length + 1);
-		arrayNew[arrayNew.length-1] = number;	
+		arrayNew[arrayNew.length - 1] = number;
 		return arrayNew;
 	}
 	
-	public static int[] insertNumberFirst(int[] array, int index, int number) {
-		int[] arraySource = {number};
-		int[] arrayNew = Arrays.copyOf(array, array.length + 1);
-		System.arraycopy(arraySource, 0, arrayNew, index, 1);
-		System.arraycopy(array, index, arrayNew, index + 1, array.length - index);	
-		return arrayNew;
-	}
+//First version of implementation:
+//	public static int[] insertNumber(int[] array, int index, int number) {
+//		int[] arraySource = { number };
+//		int[] arrayNew = Arrays.copyOf(array, array.length + 1);
+//		System.arraycopy(arraySource, 0, arrayNew, index, 1);
+//		System.arraycopy(array, index, arrayNew, index + 1, array.length - index);
+//		return arrayNew;
+//	}
+
+//Second version of implementation:
 	public static int[] insertNumber(int[] array, int index, int number) {
-		
+
 		int[] arrayNew = new int[array.length + 1];
 		arrayNew[index] = number;
 		System.arraycopy(array, 0, arrayNew, 0, index);
 		System.arraycopy(array, index, arrayNew, index + 1, array.length - index);
 		return arrayNew;
 	}
-	
+
 	public static int[] removeNumber(int[] array, int index) {
-		int[] arrayNew = new int[array.length - 1];	
+		int[] arrayNew = new int[array.length - 1];
 		System.arraycopy(array, 0, arrayNew, 0, index);
-		System.arraycopy(array, index+1, arrayNew, index, array.length - index -1);
+		System.arraycopy(array, index + 1, arrayNew, index, array.length - index - 1);
 		return arrayNew;
 	}
-	 
+
 	public static int[] insertSorted(int[] sortedArray, int number) {
 		int index = Arrays.binarySearch(sortedArray, number);
-		int insertIndex = index < 0 ? -index-1 : index;
-		return insertNumber(sortedArray, insertIndex, number); 
-		 
+		int insertIndex = index < 0 ? -index - 1 : index;
+		return insertNumber(sortedArray, insertIndex, number);
+
 	}
 
 }
-
